9bf348cf68141e17cf88315de6345a77e2a047f0
NganPham89 <algarvian2001@yahoo.com>
2023-10-30 04:10:53 -0400

    checkout page added

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 import { useState, useEffect } from "react";
 import CartItem from "./components/cart-item";
+import Summary from "./components/summary";
 
 const CartPage = () => {
     const cart = useCart();
@@ -22,7 +23,7 @@ const CartPage = () => {
         <div className="bg-white">
             <Container>
                 <div className="px-4 py-16 sm:px-6 lg:px-8">
-                    <h1 className="">Shopping Cart</h1>
+                    <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
                             {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
@@ -32,6 +33,7 @@ const CartPage = () => {
                                 ))}
                             </ul>
                         </div>
+                        <Summary />
                     </div>
                 </div>
             </Container>
